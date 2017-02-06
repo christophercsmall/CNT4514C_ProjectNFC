84633197bf64207f38a44f1bd415d4d0c468c59a
christophercsmall <christopher.c.small@gmail.com>
2017-02-06 14:44:30 -0500

    updated app to show app chooser when tag detected

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -7,13 +7,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import android.app.Activity;
+import android.nfc.NfcAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "NfcDemo";
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
 
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView_explanation);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText(R.string.explanation);
+        }
+
+        handleIntent(getIntent());
+
+    }
+
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
     }
 
     public void sendMessage(View view){
